6205693c3a8ae4b91b2f582e393d66adc9d1849c
Ankit <ak0330255@gmail.com>
2026-01-29 20:42:52 +0530

    Add new textures, HTML file, and background components
    - Added multiple new texture files for materials in the desktop_pc and planet directories.
    - Created a new index.html file for the project.
    - Added a license file for the stylized planet model.
    - Introduced a new BackgroundWrapper component to manage various background effects and elements.

---
+++ src/components/BackgroundWrapper.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StarsCanvas } from "./canvas";
+import CursorTrail from "./CursorTrail";
+import ScrollProgress, { ScrollToTop } from "./ScrollProgress";
+import ThemeToggle from "./ThemeToggle";
+import CommandPalette from "./CommandPalette";
+import FloatingActionButton from "./FloatingActionButton";
+import SpotlightEffect from "./SpotlightEffect";
+import MouseFollower from "./MouseFollower";
+import GridPattern from "./GridPattern";
+import BackgroundBeams from "./BackgroundBeams";
+import ParticleBackground from "./ParticleBackground";
+import SmoothScroll from "./SmoothScroll";
+
+const BackgroundWrapper = () => {
+  return (
+    <>
+      <div className="noise-overlay" />
+      <SmoothScroll />
+      <CursorTrail />
+      <ScrollProgress />
+      <ScrollToTop />
+      <ThemeToggle />
+      <CommandPalette />
+      <FloatingActionButton />
+      <SpotlightEffect />
+      <MouseFollower />
+      <GridPattern />
+      <BackgroundBeams />
+      <ParticleBackground />
+      <div className="aurora aurora--violet -top-32 -left-24" />
+      <div className="aurora aurora--cyan top-1/3 -right-10" />
+      <div className="aurora aurora--pink bottom-0 left-1/4" />
+      <StarsCanvas />
+    </>
+  );
+};
+
+export default BackgroundWrapper;
